be75c0243bde9366ee25840c13a619d554dc4ca6
Noureldin <noureeldinhossamm@gmail.com>
2026-04-29 11:15:25 +0300

    Merge Files

---
+++ src/components/Banner.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { ArrowDownCircle } from "react-bootstrap-icons";
+import CV from '../assets/CV.pdf'
+
+export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const [index, setIndex] = useState(1);
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta);
+        return () => { clearInterval(ticker) }
+    }, [text]);
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+        if(isDeleting) {
+            setDelta(prevDelta => prevDelta / 2);
+        }
+        if(!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setIndex(prevIndex => prevIndex - 1);
+            setDelta(period);
+        } else if(!isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setIndex(1);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        } else {
+            setIndex(prevIndex => prevIndex + 1);
+        }
+    }
+
+    const handleDownload = () => {
+        const link = document.createElement('a')
+        link.href = CV;
+        link.download = 'Noureldin_CV.pdf';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <h1>{`What's up I'm Nour, `}<span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt perspiciatis atque alias nihil labore, dolores quidem dolore quas, doloribus libero repellat voluptas totam non vero harum est consectetur ut in?</p>
+                        <button onClick={handleDownload}>Download CV <ArrowDownCircle size={25} /></button>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
+};
